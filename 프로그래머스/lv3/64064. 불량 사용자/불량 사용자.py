d7ef3ac88fd198e16afce2fb96b33a8a15b3eb63
cwelly <potato3884@naver.com>
2023-06-05 21:10:38 +0900

    [level 3] Title: 불량 사용자, Time: 0.43 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,6 +1,13 @@
 import copy
+
+#그냥 powerset 풀스캔
+
+#banned_id 를 하나 부터 for 문으로 전부 확인
+
+# 겹치는 경우는 dict형으로 관리하는 answers 에 값이 있는지로 체크
 answer = 0
 answers ={}
+# 문자열 origin 이 문자열 ban 과 매칭이 되는지 T/F로 리턴하는 메소드
 def comId (origin , ban ):
     if len(ban) != len(origin) :
         return False
@@ -8,6 +15,18 @@ def comId (origin , ban ):
         if ban[i]!='*' and ban[i]!=origin[i] :
             return False
     return True
+
+#  left_id 들의 모든 원소들을 하나씩 빼서 
+#   banned_id에 걸리면
+#       left_id 에서 빼고 banned_id 를 확인하는 인덱스를 다음값으로 넘어간다
+#   banned_id에 안 걸리면
+#       넘어간다
+
+#   체크 하다가 , banned_id를 전부 확인하면 
+#   제재 아이디를 전부 체크했으므로 left_id를 통해 dict를 확인
+#       이미 있으면 넘어가고
+#       없으면 answer 값 올리고 , dict에 추가
+
 def powerset (left_id ,  banned_id  , cur) :
     global answer
     global answers
