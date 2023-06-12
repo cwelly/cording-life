2e27e3c264989f115c1d0bc31a1cbce157b62868
cwelly <potato3884@naver.com>
2023-06-12 20:45:57 +0900

    [level 2] Title: 타겟 넘버, Time: 2.71 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,15 @@
 answer = 0
 def powerset(numbers , target , index , cur_val) :
     global answer
+    # 만약 전부 돌아봤다면
     if index  == len(numbers) :
+        # 타겟값과 같은지 비교
         if cur_val == target :
             answer+=1
         return
+    # 현재 보고있는 값을 뺐을때
     powerset(numbers , target , index+1 , cur_val - numbers[index])
+    # 현재 보고있는 값을 더할때
     powerset(numbers , target , index+1 , cur_val + numbers[index])
     # answer=1
     return 
